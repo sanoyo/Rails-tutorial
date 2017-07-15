0ad702508ef893f0b23f938112f8cb47ce25f70f
sanoyo <sa00380304yo@gmail.com>
2017-07-15 21:14:27 +0800

    Add logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,8 @@ class SessionsController < ApplicationController
     end
   end
 
-  def delete
-    
+  def destroy
+    log_out
+    redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,24 @@
 module SessionsHelper
   def log_in(user)
-    session[user_id] = user.id
+    session[:user_id] = user.id
+  end
+
+  def current_user 
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  # if @current_user.nil?
+  #   @current_user = User.find_by(id: session[:user_id])
+  # else
+  #   @current_user
+  # end
+
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
