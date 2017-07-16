ff43e80371593164efc62ad4e61b36590b7334fc
sanoyo <sa00380304yo@gmail.com>
2017-07-16 19:17:29 +0800

    Revise Code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,19 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+
+      # raise user.authenticate.inspect
+      # abort user.inspect
+
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      log_in user
+      log_in user      
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      # if params[:session][:remember_me] == '1'
+      #   remember(user)
+      # else
+      #   forget(user)
+      # end
+      
       remember user
       redirect_to user
     else
@@ -17,7 +28,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
                      uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -19,12 +20,13 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
-  def remember_token
+  def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
