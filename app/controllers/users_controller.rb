e10204b1086b82244039b519ed056746b4989e92
sanoyo <sa00380304yo@gmail.com>
2017-07-16 19:45:52 +0800

    Add Edit Form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
