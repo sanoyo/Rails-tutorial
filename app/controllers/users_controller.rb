7d15cb1d19ee0291a75008969d2d2290a00bf07b
sanoyo <sa00380304yo@gmail.com>
2017-07-16 20:13:50 +0800

    Request User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :destroy]
+  before_action :correct_user, only: [:edit, :destroy]
+
   def show
     @user = User.find(params[:id])
   end  
@@ -36,4 +39,25 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = 'Please Log in'
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
+
+
+
+
+
+
+
+
+
