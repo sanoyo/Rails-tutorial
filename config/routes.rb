46263a9f58f93ecb16b5cb21939d61005782f3ef
sanoyo <sa00380304yo@gmail.com>
2017-07-19 18:26:55 +0800

    Create  AccountActivationsController

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,5 @@ Rails.application.routes.draw do
   end
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  resources :account_activations, only: [:edit]
 end
